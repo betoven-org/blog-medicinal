473af94d5bd390bdf5878e4ee58b1adeeccd1c93
Wesley Moraes <wesleymoraesserafim@gmail.com>
2026-05-24 21:25:45 -0300

    fix: remove exports de modulos deletados do brasa-core/index

--- packages/brasa-core/src/index.ts
+++ packages/brasa-core/src/index.ts
@@ -1,6 +1,3 @@
 export * from "./schema";
 export { db } from "./db";
-export { auth, handlers, signIn, signOut } from "./auth";
-export * from "./search";
-export * from "./validations";
 export * from "./slug";
